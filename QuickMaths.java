0fbda9dc272c7ca274c883d687d0c9d4a74bc55b
AustinLeath <austinleath18@gmail.com>
2018-08-23 23:49:03 -0500

    Current version as of 8/23/18

---
+++ QuickMaths.java
@@ -0,0 +1,6 @@
+public class QuickMaths {
+  public static void main(String[] args) {
+    int number = 2;
+    System.out.println("Two plus two is" + " " + (number * 2) + " " + "minus one that's" + " " + ((number * 2) - 1) +  " " + "QUICK MATHSSS");
+  }
+}
