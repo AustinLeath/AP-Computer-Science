cee44259834e4bc3ad583f2ad0b6217cd209ef0f
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-08-28 14:57:12 -0500

    Current version as of 8/28/18

---
+++ PythagSolver.java
@@ -0,0 +1,13 @@
+import Java.util.Scanner;
+
+public class PythagSolver {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    double A;
+    double B;
+    double C;
+
+    solve = reader.nextDouble(); 
+    System.out.println();
+  }
+}
--- TempConverter.java
+++ TempConverter.java
@@ -15,7 +15,8 @@ public class TempConverter
       celsius = (fahrenheit - 32.0) * 0.5556;
 
       System.out.println("In Celsius " + fahrenheit + "\u00b0 Fahrenheit is: " + celsius + "\u00b0 Celsius");
-      System.out.println("In Kelvin " + fahrenheit + "\u00b0 Fahrenheit is: " + (fahrenheit + 273) + "\u00b0 Kelvin" );
+      System.out.println("In Kelvin " + fahrenheit + "\u00b0 Fahrenheit is: " + (fahrenheit + 273) + "\u00b0 Kelvin");
+      System.out.println("In Rankine " + fahrenheit + "\u00b0 Fahrenheit is: " + (fahrenheit - 459.67) + "\u00b0 Rankine");
     }
    }
   }
