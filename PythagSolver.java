1fc0916e4d16ab3b3ba26546bcc046099d87c894
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-08-29 09:06:12 -0500

    Current version as of 8/29/18

--- PythagSolver.java
+++ PythagSolver.java
@@ -1,13 +1,14 @@
-import Java.util.Scanner;
+import java.util.Scanner;
 
 public class PythagSolver {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
+    double listsides = 0.0;
     double A;
     double B;
     double C;
-
-    solve = reader.nextDouble(); 
-    System.out.println();
+    
+    System.out.print("Enter:" + " ");
+    listsides = reader.nextDouble();
   }
 }
