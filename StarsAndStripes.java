29a5678fa1d981a477a28ab546a022c8513c945d
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-09-20 09:10:06 -0500

    Current version as of 9/20/18

---
+++ StarsAndStripes.java
@@ -0,0 +1,63 @@
+// A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Austin Leath
+//Date - 9/18/18
+//Class - AP Computer Science
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+      out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+      out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+      out.println("\n\n");
+   }
+
+   public void printASmallBox()
+   {
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwoBlankLines();
+   }
+
+   public void printABigBox()
+  {
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwentyStars();
+    printTwentyDashes();
+    printTwoBlankLines();
+  }
+}
---
+++ StarsRunner.java
@@ -0,0 +1,21 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Austin Leath
+//Date - 9/18/18
+//Class - AP COMP SCI
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      //call the methods needed to make the patterns on the word document
+      StarsAndStripes test = new StarsAndStripes();
+      test.printASmallBox();
+      test.printABigBox();    
+   }
+}
