b495de5adbdb3b6a9debb6c4d33148b8e2649265
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-10-02 08:35:26 -0500

    Current version as of 10/2/18

---
+++ MilesPerHour.java
@@ -0,0 +1,46 @@
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class MilesPerHour
+{
+	private int distance, hours, minutes;
+	private double mph;
+
+	public MilesPerHour()
+	{
+		setNums(0,0,0);
+		mph=0.0;
+	}
+
+	public MilesPerHour(int dist, int hrs, int mins)
+	{  
+      setNums(dist,hrs,mins);
+      mph = 0.0;
+   }
+
+	public void setNums(int dist, int hrs, int mins)
+	{
+	   distance = dist;
+      hours = hrs;
+      minutes = mins;
+   }
+
+	public void calcMPH()
+	{
+      mph = distance/(hours+(minutes/60.0));
+	}
+
+	public void print()
+	{
+      out.print(distance + " miles in " + hours + 
+      " hours and " + minutes + " minutes = " + Math.round(mph) + " MPH.");
+	}
+	
+	//create a print or toString or both
+	
+	public String toString()
+	{
+		return "";
+	}
+}
