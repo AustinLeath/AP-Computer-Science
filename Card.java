372fa7a892bce3820d359d8381a5a682f50c5ea4
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-10-03 14:42:36 -0500

    Current version as of 10/3/18

---
+++ Card.java
@@ -0,0 +1,38 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Austin Leath
+
+public class Card
+{
+	public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
+			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+
+	private String suit;
+	private int face;
+
+	public Card(String s, int f) {
+		setSuit(s);
+		setFace(f);
+	}
+
+	public void setSuit(String s) {
+		suit = s;
+	}
+
+	public void setFace(int f) {
+		face = f;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public int getFace() {
+		return face;
+	}
+
+	public String toString() {
+		return (FACES[face] + " of " + suit);
+	}
+
+ }
