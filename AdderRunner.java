7ea0ac1212f1a2f2017ba0f8958bc31c4484f12e
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-10-04 12:54:08 -0500

    Current version as of 10/4/18

---
+++ AdderRunner.java
@@ -0,0 +1,12 @@
+public class AdderRunner {
+  public static void main(String[] args) {
+    Adder test = new Adder();
+    test.setNums(32,21);
+    test.add();
+    test.print();
+
+    test.setNums(15,8);
+    test.add();
+    test.print();
+  }
+}
---
+++ DistanceRunner.java
@@ -0,0 +1,18 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Austin Leath
+
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class DistanceRunner
+{
+	public static void main( String[] args )
+	{
+		Distance test = new Distance();
+		test.calcDistance();
+
+	}
+}
