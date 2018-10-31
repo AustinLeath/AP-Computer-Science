77f545f738476e7d63b420b143b97efb5b8ea87f
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-10-31 09:01:02 -0500

    Current version as of 10/31/18

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,22 +3,6 @@ import java.util.Scanner;
 public class HelloWorld {
   public static void main(String[] args) {
 
-      Scanner keyboard = new Scanner(System.in);
-
-      System.out.print("Enter your new password: ");
-      String passwordone = keyboard.next();
-
-      System.out.print("Re-enter your password: ");
-      String passwordtwo = keyboard.next();
-
-      if(passwordone.equals(passwordtwo)) {
-         System.out.println("Thank you! your password matches!");
-      } else {
-         System.out.println("Sorry, the password you entered was not identical to the one you created please try again.");
-      }
-
-      System.out.print("Create a bio:");
-      String biography = keyboard.next();
-
+      System.out.print("Hello World");
     }
 }
