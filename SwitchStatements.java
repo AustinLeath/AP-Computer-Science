e6eb2a1aad89120fc504d6ff1cfe637e57910951
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-11-02 08:41:42 -0500

    Current version as of 11/2/18

---
+++ SwitchStatements.java
@@ -0,0 +1,13 @@
+public class SwitchStatements {
+  public static void main(String[] args) {
+    int num = 30;
+
+    switch(num) {
+      case 11: System.out.println("num == 11"); break;
+
+      case 30: System.out.println("num == 30"); break;
+
+      case 21: System.out.println("num == 21"); break;
+    }
+  }
+}
