ef7e5024cba44a411ba3d13bc56a37dc1d0471b4
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-11-13 12:00:35 -0600

    Current version as of 11/13/18

---
+++ CoolNumbers.java
@@ -0,0 +1,26 @@
+import static java.lang.System.*;
+
+public class CoolNumbers
+{
+	public static boolean isCoolNumber( int num )
+	{
+		 if (num % 3 == 1 && num % 4 ==1 && num % 5 ==1 && num % 6 == 1 )
+		      return true;
+		 return false;
+	}
+
+	public static int countCoolNumbers( int stop )
+	{
+		int count = 0;
+
+		for (int num = 6;  num <= stop;  num++)
+		{
+			   if(isCoolNumber(num))
+			   {
+			   		count = count + 1;
+
+			   }
+		}
+		return count;
+	}
+}
