3dec9eb3cef99e5325c741cc8a079786e7bc2c4b
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-12-06 08:27:48 -0600

    Current version as of 12/6/18

--- C/main.c
+++ C/main.c
@@ -1,29 +1,9 @@
-/*
-int main() {
-   double test = 1.0;
-        if (test == 1.0) {
-            printf("1\n");
-    }
-        if (test == 2.0) {
-            printf("2\n");
-    }
-        if (test == 3.0) {
-            printf("3\n");
-    }
-        if (test == 4.0) {
-            printf("4\n");
-    }
-}
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
- 
+
 int main()
 {
-    long int hat;
-    hat = rand(); /*use rand() for short ints*/
-    printf("%ld is a random number.\n",hat);
+    printf("Hello World");
     return(0);
 }
