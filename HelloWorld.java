f2d1f5061bab12aef23ef21201d12eed9c8f0229
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-12-11 12:46:41 -0600

    Current version as of 12/11/18

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,6 @@ import java.lang.System;
 import java.util.Scanner;
 public class HelloWorld {
   public static void main(String[] args) {
-    int c = 5;
-if( c>3 && c>10 )
-  System.out.print("it");
-System.out.print("up");
+    System.out.print("Hello World");
   }
 }
