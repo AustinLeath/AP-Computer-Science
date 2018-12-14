56f36f61dd325804c0e4e65fcfd0f8c2bbcd22c7
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-12-14 08:41:10 -0600

    Current version as of 12/14/18

---
+++ Box.java
@@ -0,0 +1,45 @@
+import static java.lang.System.*;
+
+public class Box
+{
+  private int size;
+
+	public Box()
+	{
+	}
+
+	public Box(int count)
+	{
+    setSize(count);
+  }
+
+	public void setSize(int count)
+	{
+    size = count;
+	}
+
+	public int getSize()
+	{
+    count.getSize();
+		return 0;
+	}
+
+	public String toString()
+	{
+		String output="test";
+    /*
+    {
+    loop j
+    {
+    output += "*";
+    }
+    loop k
+    {
+    output += "#";
+    }
+    output += "\n";
+    }
+    */
+		return output;
+	}
+}
---
+++ BoxRunner.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+
+public class BoxRunner
+{
+   public static void main( String args[] )
+   {
+     Box input = new Box();
+
+     input.setSize(4);
+
+	 }
+}
