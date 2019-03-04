ac32e7f1b36d5d8e144951f3e838f4a8e607943c
AustinLeath <austinleath18@gmail.com>
2019-03-04 00:03:01 -0600

    Update test.java

--- test.java
+++ test.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.io.File;
 import java.util.Scanner;
-
+import java.util.Arrays;
 
 public class test {
    public static void main(String[] args) throws IOException {
-      Scanner file = new Scanner( new File("nums.dat"));
-      System.out.println(file.nextInt() + " " + file.nextLine());
+
    }
 }
