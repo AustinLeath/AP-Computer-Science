1d93db8924aa0a2206d3220e1170f371d719a00f
Austin Leath <austinleath18@gmail.com>
2021-05-10 13:00:14 -0500

    Update RaySmallest.java

--- Semester II/RaySmallest.java
+++ Semester II/RaySmallest.java
@@ -1,26 +1,17 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name - Austin Leath
-//Date -
-
 public class RaySmallest
 {
 	public static int go(int[] ray)
 	{
-   int smallest = Integer.MAX_VALUE;
-   for(int item : ray)
+		int smallest = Integer.MAX_VALUE;
+		for(int item : ray)
 		{
-        int num = item;
-          if(num<smallest)
-          {
-            smallest=num;
-          }
+			int num = item;
+			  if(num<smallest)
+			  {
+			    smallest=num;
+			  }
 		}
-      System.out.println(smallest + " ");
-      return 0;
+		System.out.println(smallest + " ");
+		return 0;
 	}
 }
-
-
-
-
